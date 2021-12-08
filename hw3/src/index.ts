55dd89ad83b116178fbf14eba783f8a5f614d10e
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2021-12-08 23:35:26 +0300

    add check insert

--- hw3/src/index.ts
+++ hw3/src/index.ts
@@ -1,4 +1,6 @@
 import {Tree} from "./binary-search-tree/tree.js";
 
-let tree: Tree = new Tree();
-console.log(tree);
+let tree: Tree<number> = new Tree<number>();
+tree.insert(11);
+tree.insert(7);
+tree.print();
