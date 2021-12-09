c5de71608ed65b5310862e065a8bcf1b3b29005c
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2021-12-09 23:54:47 +0300

    ts build, that need to filling view

---
+++ hw3/build/index.js
@@ -0,0 +1,1 @@
+import{Tree}from"./binary-search-tree/tree.js";function printTree(){let e=new Tree;e.insert(7,"7"),e.insert(3,"3"),e.insert(9,"9"),e.insert(2,"2"),e.insert(5,"5"),e.insert(8,"8"),e.insert(10,"10"),e.insert(1,"1"),e.insert(6,"6"),e.print("h")}export{printTree};
