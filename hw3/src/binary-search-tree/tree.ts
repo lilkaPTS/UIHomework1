75bc7634e3ce8fccc81035018710796161104267
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2021-12-09 03:00:48 +0300

    TreeSet -> TreeMap & added get()

--- hw3/src/binary-search-tree/tree.ts
+++ hw3/src/binary-search-tree/tree.ts
@@ -1,46 +1,107 @@
-class Node<T> {
-    private _data: T;
-    private _lChild: Node<T> | null;
-    private _rChild: Node<T> | null;
+import {createSocket} from "dgram";
 
-    constructor(data: T, lChild: Node<T> | null, rChild: Node<T> | null) {
+class Node<K, E> {
+    private _key: K;
+    private _data: E;
+    private _lChild: Node<K, E> | null;
+    private _rChild: Node<K, E> | null;
+
+
+    constructor(key: K, data: E, lChild: Node<K, E> | null, rChild: Node<K, E> | null) {
+        this._key = key;
         this._data = data;
         this._lChild = lChild;
         this._rChild = rChild;
     }
 
-    get data(): T {
+    get key(): K {
+        return this._key;
+    }
+
+    set key(value: K) {
+        this._key = value;
+    }
+
+    get data(): E {
         return this._data;
     }
 
-    set data(value: T) {
+    set data(value: E) {
         this._data = value;
     }
 
-    get lChild(): Node<T> | null {
+    get lChild(): Node<K, E> | null {
         return this._lChild;
     }
 
-    set lChild(value: Node<T> | null) {
+    set lChild(value: Node<K, E> | null) {
         this._lChild = value;
     }
 
-    get rChild(): Node<T> | null {
+    get rChild(): Node<K, E> | null {
         return this._rChild;
     }
 
-    set rChild(value: Node<T> | null) {
+    set rChild(value: Node<K, E> | null) {
         this._rChild = value;
     }
 }
 
-export class Tree<T> {
-    private _root: Node<T> | null;
+export class Tree<K, E> {
+    private _root: Node<K, E>  | null;
 
     constructor() {
         this._root = null;
     }
 
+    get(key: K): E | null {
+        let currentNode: Node<K, E> | null = this._root;
+        while (true) {
+            if(currentNode == null){
+                return null;
+            } else if(key == currentNode.key) {
+                return currentNode.data;
+            } else if (key < currentNode.key) {
+                currentNode = currentNode.lChild;
+            } else {
+                currentNode = currentNode.rChild;
+            }
+        }
+    }
+
+    remove(key:K): void {
+
+    }
+
+
+    insert(key: K, data: E): void {
+        let newNode: Node<K, E> = new Node<K, E>(key, data, null, null);
+        if(this._root == null) {
+            this._root = newNode;
+            return;
+        }
+        let currentNode: Node<K, E> = this._root;
+        while (true) {
+            if(key == currentNode.key) {
+                return;
+            } else if(key < currentNode.key) { //left
+                if(currentNode.lChild == null) {
+                    currentNode.lChild = newNode;
+                    return;
+                } else {
+                    currentNode = currentNode.lChild;
+                }
+            } else {                            //right
+                if(currentNode.rChild == null) {
+                    currentNode.rChild = newNode;
+                    return;
+                } else {
+                    currentNode = currentNode.rChild;
+                }
+            }
+        }
+    }
+    /*
     insert(data: T): void {
         let newNode: Node<T> = new Node<T>(data, null, null);
         if(this._root == null) {
@@ -68,6 +129,7 @@ export class Tree<T> {
             }
         }
     }
+    */
 
     print(): void {
         if(this._root != null)
@@ -75,7 +137,7 @@ export class Tree<T> {
         else return;
     }
 
-    private printHelper(node : Node<T>): void {
+    private printHelper(node : Node<K,E>): void {
         console.log("\t%s\n%s\t\t%s", node.data, node.lChild?.data, node.rChild?.data);
 
         if(node.lChild != null && node.rChild != null) {
--- hw3/src/index.ts
+++ hw3/src/index.ts
@@ -1,6 +1,23 @@
 import {Tree} from "./binary-search-tree/tree.js";
+import {Key} from "readline";
 
-let tree: Tree<number> = new Tree<number>();
-tree.insert(11);
-tree.insert(7);
-tree.print();
+let tree: Tree<number,string> = new Tree<number, string>();
+tree.insert(7,"H");
+tree.insert(3,"E");
+tree.insert(9,"L");
+tree.insert(2,"L");
+tree.insert(5,"O");
+tree.insert(8,"T");
+tree.insert(1,"R");
+tree.insert(4,"E");
+tree.insert(6,"E");
+console.log(tree.get(7));
+console.log(tree.get(3));
+console.log(tree.get(9));
+console.log(tree.get(2));
+console.log(tree.get(5));
+console.log(tree.get(8));
+console.log(tree.get(1));
+console.log(tree.get(4));
+console.log(tree.get(6));
+
