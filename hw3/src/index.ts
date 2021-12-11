a4a172916b6f6b7c82054195fe4540764eba2118
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2021-12-11 16:30:58 +0300

    readlineSync does not work

--- hw3/src/index.ts
+++ hw3/src/index.ts
@@ -1,6 +1,6 @@
 import {TreeByConsole} from "./binary-search-tree/treeByConsole.js";
-
 let tree: TreeByConsole<number,string> = new TreeByConsole<number, string>();
+
 tree.insert(8, "8");
 tree.insert(4, "4");
 tree.insert(2, "2");
@@ -13,3 +13,13 @@ tree.insert(3, "3");
 tree.insert(7, "7");
 tree.print();
 
+/* // не работает вместе с импортом дерева требуется commonJs, а для импорта module
+var readlineSync = require('readline-sync');
+// Wait for user's response.
+var userName:string = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+// Handle the secret text (e.g. password).
+var favFood:string  = readlineSync.question('What is your favorite food?');
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+ */
